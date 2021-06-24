b42fac799d2ad9aadb8a458d509e628b8bfabfcd
Medjadder Aimen <aimen.medjadder@vitalcareprod.com>
2021-06-24 19:38:42 +0100

    push to netlify

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <select
+    @change="onChange"
+    v-model="selected"
+    class="form-select block w-full border rounded mt-10"
+  >
+    <option value="0">select country</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountryScript",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats"/>
+    <country-selecet :countries="countries" @get-country="getCountryData" />
+    <button 
+    @click="clearCountryData"
+    v-if="stats.Country" 
+    class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+      Clear countries
+    </button>
+  </main>
+  <main v-else class="felx flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      fetching data
+    </div>
+    <img :src="loadingImage" alt="" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import DataTitle from '../components/DataTitle.vue'
+import DataBoxes from '../components/DataBoxes.vue'
+import CountrySelecet from '../components/CountrySelect.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTitle,
+    DataBoxes,
+    CountrySelecet
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+
+      const data = res.json();
+
+      return data;
+    },
+    getCountryData(country){
+
+      this.stats=country
+      this.title=country.country
+
+    },
+    async clearCountryData(){
+
+        this.loading=true
+        const data = await this.fetchCovidData()
+        this.title= 'Global'
+        this.stats=data.Global
+        this.loading=false
+
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
